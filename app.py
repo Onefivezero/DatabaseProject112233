aa4abd222f908cf98a95266b11b3a49d30db994f
Onefivezero <43936380+Onefivezero@users.noreply.github.com>
2021-01-19 23:57:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -270,7 +270,8 @@ def deletequery():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     cur.execute("select * from information_schema.table_constraints")
-    print("YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" + cur.fetchall())
+    print("mama mia")
+    print(cur.fetchall())
     if request.method == "POST":
         user = load_user(request.form['givenu'])
         if user:
