517d9a3edc390e0f70e45b62a5df3636517a89f1
Onefivezero <43936380+Onefivezero@users.noreply.github.com>
2021-01-20 03:05:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -243,9 +243,10 @@ def register():
         query_data = []
         for i in protodata:
             try:
+                data.append(i[0])
                 cur.execute("SELECT name, lecture_code, day, hours FROM courses WHERE crn = %s", (i[0],))
-                temp = list(cur.fetchone())
-                data += temp
+                temp = cur.fetchone()
+                data.append(temp)
                 query_data.append(temp[0])
             except Exception as err:
                 print(err)
